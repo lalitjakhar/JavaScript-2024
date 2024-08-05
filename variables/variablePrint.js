0c45529e0402259caca6c4ebad9d294de52ec5c2
lalitjakhar <lalit25jakhar@gmail.com>
2024-08-05 21:27:53 +0530

    Variable Declare and Use

---
+++ variables/variablePrint.js
@@ -0,0 +1,14 @@
+//Declare variable
+var firstName = "Lalit";
+//use a variable
+console.log(firstName);
+
+//Variable are case sensative
+
+//Change Value
+
+firstName = "Jakhar";
+
+//Print Again
+
+console.log(firstName);
