140c14f62f9a1eed5fe590e0b3dd1c5708c06015
lalitjakhar <lalit25jakhar@gmail.com>
2024-08-05 21:41:09 +0530

    Rules for Naming Variables

---
+++ variables/naming-var/namingVar.js
@@ -0,0 +1,26 @@
+// rules for naming
+
+// can't start with number
+
+// example:-
+// 1value (invalid)
+// value1 (valid)
+
+var value1 = 10;
+console.log(value1 / 2);
+
+// can use only _ or $ sign
+
+// example :-
+// first_name (valid)
+// _firstname (valid)
+// first$name (valid)
+// $firstname (valid)
+
+// cant use spaces
+// first name (invalid)
+
+// var first_name = "Jakhar" //snake case writing
+// var firstName = "Jakhar" //camel case writing
+
+//start with small letter and use CamelCase
