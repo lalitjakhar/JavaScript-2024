688aa49b3f16ba31c57888ce7e1e1244a1217334
lalitjakhar <lalit25jakhar@gmail.com>
2024-08-26 15:49:55 +0530

    while loop

---
+++ while-loop/WhileLoop.js
@@ -0,0 +1,75 @@
+// print number 0 to 9
+
+//method first
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+
+// method second
+// let i = 0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+
+//third Method while loop
+
+// let i = 0;
+// while (i <= 9) {
+//   console.log(i);
+//   i++;
+// }
+
+// sum of 10 number from 0 to 10
+//1+2+3+4+5+6+7+8+9+10
+
+let num = 10;
+let total = 0;
+
+//method first
+// total = total + 1;
+// total = total + 2;
+// total = total + 3;
+// total = total + 4;
+// total = total + 5;
+// total = total + 6;
+// total = total + 7;
+// total = total + 8;
+// total = total + 9;
+// total = total + 10;
+
+// console.log(total);
+
+//method second while loop
+let i = 0;
+while (i <= 10) {
+  total = total + i;
+  i++;
+}
+console.log(total);
+
+// method two using maths
+// let total = (num * (num + 1)) / 2;
+// console.log(total);
