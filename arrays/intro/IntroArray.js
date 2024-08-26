c0b70b2d86e7dc96df7e37ead5b68c7127f94718
lalitjakhar <lalit25jakhar@gmail.com>
2024-08-26 17:04:16 +0530

    arrays intro

---
+++ arrays/intro/IntroArray.js
@@ -0,0 +1,27 @@
+// intro to arrays
+// ordered collection of items
+
+// reference type
+
+// how to create arrays
+
+let fruits = ["apple", "banana", "grapes"];
+
+console.log(fruits[2]);
+
+let numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+
+let mixed = [1, "string", null, undefined, 5, 6];
+console.log(mixed);
+
+let fruits2 = ["apple", "banana", "grapes"];
+console.log(fruits2);
+fruits2[1] = "mango";
+console.log(fruits2);
+
+let fruits3 = ["apple", "banana", "grapes"];
+console.log(typeof fruits3);
+console.log(Array.isArray(fruits3));
+
+// array indexing
