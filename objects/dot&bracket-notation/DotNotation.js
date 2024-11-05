5df5314c6e48fb02f472db4e1fcb2df22725b60d
lalitjakhar <lalit25jakhar@gmail.com>
2024-11-05 12:47:06 +0530

    objects in js intro

---
+++ objects/dot&bracket-notation/DotNotation.js
@@ -0,0 +1,13 @@
+// diffrence b/w dot and bracket notation
+
+const person = {
+  name: "Jakhar",
+  age: 18,
+  "person hobbies": ["sleeping", "listening music"],
+};
+
+//  dot notation
+console.log(person.age);
+
+// bracket notation
+console.log(person["person hobbies"]);
---
+++ objects/intro/ObjectIntro.js
@@ -0,0 +1,23 @@
+// objects - reference type
+// arrays are good but not sufficient for real world Data
+// objects store key value pairs
+// objects don't have index
+
+// how to create objects
+
+const person = {
+  name: "Jakhar",
+  age: 18,
+  hobbies: ["sleeping", "listening music"],
+};
+console.log(person);
+
+// how to access data from objects
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies);
+
+// how to add key value pair to objects
+person.gender = "male";
+console.log(person.gender);
