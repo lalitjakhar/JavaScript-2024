1e71525ba3501a4532a2db8c3ed599b5a541b7b8
lalitjakhar <lalit25jakhar@gmail.com>
2024-11-08 14:48:46 +0530

    Arrays methods in js

---
+++ arrays/array-methods/fill-method/Fill.js
@@ -0,0 +1,8 @@
+// fill method
+
+const myArray = new Array(10).fill(-1);
+console.log(myArray);
+
+const myArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+myArray.fill(0, 2, 4);
+console.log(myArray);
--- arrays/array-methods/some/Some.js
+++ arrays/array-methods/some/Some.js
@@ -1,17 +1,16 @@
-// every method
+// some method
 
-const numbers = [2, 4, 6, 8, 10];
+const numbers = [3, 5, 8, 9];
 
-const ans = numbers.every((number) => number % 2 === 0);
+const ans = numbers.some((number) => number % 2 === 0);
 console.log(ans);
 
-// other example
-
+//other example
 const userCart = [
   { productId: 1, productName: "mobile", price: "33000" },
   { productId: 2, productName: "laptop", price: "122000" },
   { productId: 3, productName: "tv", price: "18000" },
   { productId: 4, productName: "refrigator", price: "12000" },
 ];
-const answer = userCart.every((cartItem) => cartItem.price < 30000);
+const answer = userCart.some((cartItem) => cartItem.price < 100000);
 console.log(answer);
