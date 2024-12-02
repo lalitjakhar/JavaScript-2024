c4df67fe745237e8a664da2ca64f464f1a99bed8
Lalit Choudhary <137052617+lalitjakhar@users.noreply.github.com>
2024-12-02 12:53:49 +0530

    change ui

--- objects/intro/ObjectIntro.js
+++ objects/intro/ObjectIntro.js
@@ -15,7 +15,7 @@ console.log(person);
 // how to access data from objects
 
 console.log(person.name);
-console.log(person.age);
+// console.log(person.age);
 console.log(person.hobbies);
 
 // how to add key value pair to objects
