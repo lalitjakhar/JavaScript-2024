017eded7f17a9fbbc8a772da235b57399f77c94c
Lalit Choudhary <137052617+lalitjakhar@users.noreply.github.com>
2024-12-27 17:34:45 +0530

    splice method

--- functions/arrow-functions/ArrowFunctions.js
+++ functions/arrow-functions/ArrowFunctions.js
@@ -1,7 +1,7 @@
 // Arrow Function
 
 const singHappyBirthday = () => {
-  console.log("Happy Birthday To You........................asdfsdfdvsdfvergerdfvdf");
+  console.log("Happy Birthday To You.......😍");
 };
 singHappyBirthday();
 
