4c0ca85d5cb6c95e8bbcaf9bff3c7e931022acd9
Lalit Choudhary <137052617+lalitjakhar@users.noreply.github.com>
2025-02-03 17:43:14 +0530

    Code Fixed for Computed Properties

--- objects/computed-properties/ComputedProperties.js
+++ objects/computed-properties/ComputedProperties.js
@@ -16,6 +16,15 @@ const value2 = "valuekey2";
 //   [key2]: value2,
 // };
 
+// const obj = {
+//   [key1]: value1,
+//   [key2]: value2,
+// };
+// const obj = {
+//   [key1]: value1,
+//   [key2]: value2,
+// };
+
 const obj = {};
 
 obj[key1] = value1;
